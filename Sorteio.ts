a5067417169db5106142386880396706abe43bf0
Bruno Gonçalves <60451767+brugoncalvesss@users.noreply.github.com>
2021-08-17 08:57:34 -0300

    Update Sorteio.ts

--- Sorteio.ts
+++ Sorteio.ts
@@ -1,1 +1,21 @@
+class Sorteio {
+    private _lista: string[] = [];
 
+    private get sortear(): number {
+        let max = this._lista.length;
+        
+        return Math.floor(Math.random() * (max - 0) + 0);
+    }
+
+    public set participar(value: string) {
+        this._lista.push(value);
+    }
+
+    public get participantes(): string[] {
+        return this._lista;
+    }
+
+    public get resultado(): string {
+        return this._lista[this.sortear];
+    }
+}
